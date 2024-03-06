183a1b22b78faefa30c2bd4410dec5d9f674f4db
Eduard <leonardodavinchee@gmail.com>
2024-03-06 20:42:09 +0500

    Initial commit

---
+++ src/main/java/com/around/aroundcore/AroundCoreApplication.java
@@ -0,0 +1,13 @@
+package com.around.aroundcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroundCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/around/aroundcore/AroundCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.around.aroundcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
