e42d83327be9fa7a397d87c05d32d70f93f2d73f
nikita <nikitapopov1295@gmail.com>
2024-04-21 14:46:46 +0500

    Added H3ChunkService + test

--- src/main/java/com/around/aroundcore/config/AroundConfig.java
+++ src/main/java/com/around/aroundcore/config/AroundConfig.java
@@ -1,9 +1,14 @@
 package com.around.aroundcore.config;
 
+import com.around.aroundcore.web.mappers.StringGameChunkDTOMapper;
+import com.around.aroundcore.web.services.H3ChunkService;
+import com.uber.h3core.H3Core;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.io.IOException;
 
 @Configuration
 @AllArgsConstructor
@@ -26,5 +31,17 @@ public class AroundConfig {
         threadPoolTaskScheduler.setThreadNamePrefix(
                 "ThreadPoolTaskScheduler");
         return threadPoolTaskScheduler;
+    }
+    @Bean
+    public H3Core h3Core() throws IOException {
+        return H3Core.newInstance();
+    }
+    @Bean
+    public StringGameChunkDTOMapper stringGameChunkDTOMapper(){
+        return new StringGameChunkDTOMapper();
+    }
+    @Bean
+    public H3ChunkService h3ChunkService() throws IOException {
+        return new H3ChunkService(h3Core(),stringGameChunkDTOMapper());
     }
 }
